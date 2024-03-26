260ac72531e839fcfb00c2e4da78cc16a3b28c23
ACGMN <ootopoo@vip.qq.com>
2024-03-26 11:34:14 +0800

    1
    Signed-off-by: ACGMN <ootopoo@vip.qq.com>

--- python/src/main.cc
+++ python/src/main.cc
@@ -37,7 +37,8 @@ void init_triton_ir(pybind11::module &&m);
 void init_triton_llvm(pybind11::module &&m);
 void init_triton_interpreter(pybind11::module &&m);
 void init_triton_passes(pybind11::module &&m);
-FOR_EACH_P(DECLARE_BACKEND, TRITON_BACKENDS_TUPLE)
+void init_triton_nvidia(pybind11::module &&m);
+// FOR_EACH_P(DECLARE_BACKEND, TRITON_BACKENDS_TUPLE)
 
 PYBIND11_MODULE(libtriton, m) {
   m.doc() = "Python bindings to the C++ Triton API";
@@ -46,5 +47,6 @@ PYBIND11_MODULE(libtriton, m) {
   init_triton_passes(m.def_submodule("passes"));
   init_triton_interpreter(m.def_submodule("interpreter"));
   init_triton_llvm(m.def_submodule("llvm"));
-  FOR_EACH_P(INIT_BACKEND, TRITON_BACKENDS_TUPLE)
+  init_triton_nvidia(m.def_submodule("nvidia"));
+  // FOR_EACH_P(INIT_BACKEND, TRITON_BACKENDS_TUPLE)
 }
